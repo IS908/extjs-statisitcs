2a58d7ceaa1c534c7745988a36c82173be3f3f3b
IS908 <1196355049@qq.com>
2015-12-09 20:55:34 +0800

    modify addProject.js -- edit window

--- src/main/webapp/app/view/prj_treeList/addProject.js
+++ src/main/webapp/app/view/prj_treeList/addProject.js
@@ -8,11 +8,12 @@ Ext.define('App.view.prj_treeList.addProject', {
     alias: 'widget.prjAdd',
 
 
-    width: 400,
-    height: 300,
+    width: 280,
+    height: 220,
     id: 'addPrj',
     autoScroll: true,
-    layout: 'fit',
+    layout: 'border',
+    border: false,
     title: '添加项目',
     //html: '<iframe style="overflow:auto;width:100%; height:100%;" src="http://www.baidu.com" frameborder="0"></iframe>'
 
@@ -23,28 +24,38 @@ Ext.define('App.view.prj_treeList.addProject', {
             items: [
                 {
                     xtype: 'form',
+                    margin: '15 0 0 15',
+                    border: false,
                     items: [
                         {
                             xtype: 'textfield',
+                            border: false,
                             fieldLabel: '工程名称'
                         }, {
                             xtype: 'textfield',
+                            border: false,
                             fieldLabel: '工程版本'
                         }, {
                             xtype: 'textfield',
+                            border: false,
                             fieldLabel: '代码行数'
                         }, {
                             xtype: 'textfield',
+                            border: false,
                             fieldLabel: '代码活跃度'
                         }, {
                             xtype: 'textfield',
+                            border: false,
                             fieldLabel: '源码地址'
                         }
                     ]}
             ],
             buttons: [{
                 text: '确认',
-                id: 'btnOK'
+                id: 'btnOK',
+                handler: function() {
+                    me.close();
+                }
             }, {
                 text: '关闭',
                 id: 'close',
