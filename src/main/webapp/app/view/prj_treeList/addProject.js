cb2ace85915b8d9b5119760c3ba495b235c3dcd0
IS908 <1196355049@qq.com>
2015-12-09 20:33:50 +0800

    modify addProject.js -- edit window

--- src/main/webapp/app/view/prj_treeList/addProject.js
+++ src/main/webapp/app/view/prj_treeList/addProject.js
@@ -8,25 +8,51 @@ Ext.define('App.view.prj_treeList.addProject', {
     alias: 'widget.prjAdd',
 
 
-    height: 300,
     width: 400,
+    height: 300,
+    id: 'addPrj',
     autoScroll: true,
-    layout: 'border',
+    layout: 'fit',
     title: '添加项目',
-    html: '<iframe style="overflow:auto;width:100%; height:100%;" src="http://www.baidu.com" frameborder="0"></iframe>'
+    //html: '<iframe style="overflow:auto;width:100%; height:100%;" src="http://www.baidu.com" frameborder="0"></iframe>'
 
-    /*initComponent: function() {
+    initComponent: function () {
         var me = this;
 
         Ext.applyIf(me, {
-
-            items: [{
-                    xtype: 'label',
-                    text: 'COBOT版本'
-                }]
+            items: [
+                {
+                    xtype: 'form',
+                    items: [
+                        {
+                            xtype: 'textfield',
+                            fieldLabel: '工程名称'
+                        }, {
+                            xtype: 'textfield',
+                            fieldLabel: '工程版本'
+                        }, {
+                            xtype: 'textfield',
+                            fieldLabel: '代码行数'
+                        }, {
+                            xtype: 'textfield',
+                            fieldLabel: '代码活跃度'
+                        }, {
+                            xtype: 'textfield',
+                            fieldLabel: '源码地址'
+                        }
+                    ]}
+            ],
+            buttons: [{
+                text: '确认',
+                id: 'btnOK'
+            }, {
+                text: '关闭',
+                id: 'close',
+                handler: function() {
+                    me.close();
+                }
+            }]
         });
-
         me.callParent(arguments);
-    }*/
-
+    }
 });
